ba9c8b80d1a1db16db7543029b94029f7312f620
Marc Juul <github@juul.io>
2017-12-02 22:40:10 -0800

    fix X init

--- config/root/x.sh
+++ config/root/x.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-modprobe eink_fb_waveform
-modprobe eink_fb_hal
-modprobe mxc_epdc_fb
-modprobe eink_fb_hal_fslepdc # optional bootstrap=0
-modprobe eink_fb_shim
 DISPLAY=:0 Xorg -modulepath /usr/lib/xorg/modules,/usr/lib/arm-linux-gnueabihf,/usr/lib/arm-linux-gnueabihf/xorg/modules/drivers
-
