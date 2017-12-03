300f4ffcebf52e4e1c0b2014202121fe522beaf9
Marc Juul <github@juul.io>
2017-12-02 20:37:54 -0800

    more internet script fix

--- config/root/internet.sh
+++ config/root/internet.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 if [ "$#" -lt "1" ]; then
-  echo "Usage: $0 <gateway_ip>" > /dev/stderr
+  echo "Usage: $0 <gateway_ip>"
   exit 1
 fi 
 
 echo "Setting default route for usb0 to $1"
 ip addr flush dev usb0
-ip addr add 192.168.1.1 dev usb0
+ip addr add 192.168.1.1/24 dev usb0
 ip route add default via $1 dev usb0
 
 echo "Setting nameserver to 8.8.8.8"
-echo "8.8.8.8" > /etc/resolv.con
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
 
