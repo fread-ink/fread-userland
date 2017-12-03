73573394e8d768a19acbbfa36f25e9df8117fe48
Marc Juul <github@juul.io>
2017-12-02 20:07:53 -0800

    add simple script to access internet via usb0

---
+++ config/root/internet.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [ "$#" -lt "1" ]; then
+  echo "Usage: $0 <gateway_ip>" > /dev/stderr
+  exit 1
+fi 
+
+echo "Setting default route for usb0 to $1"
+ip addr flush dev usb0 && ip route add default via $1 dev usb0
+
+echo "Setting nameserver to 8.8.8.8"
+echo "8.8.8.8" > /etc/resolv.con
+
